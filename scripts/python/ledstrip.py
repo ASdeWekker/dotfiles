792296b8fb872a8cb18bf87104ea87523923e765
Alex de Wekker <github@dewekker.dev>
2020-02-23 11:22:55 +0100

    added some comments

--- scripts/python/ledstrip.py
+++ scripts/python/ledstrip.py
@@ -1,5 +1,6 @@
 #!/bin/python
 
+# Some imports.
 import argparse
 import requests
 
@@ -9,6 +10,7 @@ ip = "http://192.168.1.220/"
 
 # Argparse configuration.
 parser = argparse.ArgumentParser(description="Control this ledstrip.")
+# Parser arguments, should be explained by the help text.
 parser.add_argument("-p", "--power", help="To turn on or off.")
 parser.add_argument("-c", "--color", help="Choose a color.")
 parser.add_argument("-R", "--rgb", help="Choose a color via rgb value.")
@@ -19,35 +21,42 @@ parser.add_argument("-f", "--fade", help="Let the current color fade in and out 
 parser.add_argument("-w", "--wakeup", help="Turn on the wakeup function.", action="store_true")
 args = parser.parse_args()
 
-# Check if the power argument was passed and execute the function.
+# Check if the power argument was passed and turn it on or off.
 if args.power:
 	response = requests.post(ip + "power", data={"power": str(args.power)})
 	print(response.text)
 
+# Check if the color argument was passed and change the color.
 if args.color:
 	response = requests.post(ip + "color", data={"color": str(args.color)})
 	print(response.text)
 
+# Check if the rgb argument was passed and change the color based on an rgb color value.
 if args.rgb:
 	response = requests.post(ip + "rgb", data={"rgb": str(args.rgb)})
 	print(response.text)
 
+# Check if the hsv argument was passed and .
 if args.hsv:
 	response = requests.post(ip + "hsv", data={"hsv": str(args.hsv)})
 	print(response.text)
 
+# Check if the brightness argument was passed and change the brightness.
 if args.brightness:
 	response = requests.post(ip + "brightness", data={"brightness": str(args.brightness)})
 	print(response.text)
 
+# Check if the rainbow argument was passed and turn on the rainbow with the specified delay.
 if args.rainbow:
 	response = requests.post(ip + "rainbow", data={"speed": str(args.rainbow)})
 	print(response.text)
 
+# Check if the fade argument was passed and fade the color with the specified delay.
 if args.fade:
 	response = requests.post(ip + "fade", data={"delay": str(args.fade)})
 	print(response.text)
 
+# Check if the wakeup argument was passed and start the wake up function.
 if args.wakeup:
 	response = requests.post(ip + "wakeup")
 	print(response.text)
