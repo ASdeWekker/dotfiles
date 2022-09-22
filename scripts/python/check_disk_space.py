78593198c91fbfd03e5b7ad0017721b2f672f996
Alex de Wekker <github@dewekker.dev>
2022-09-22 23:21:11 +0200

    fixed config for apprise

--- scripts/python/check_disk_space.py
+++ scripts/python/check_disk_space.py
@@ -5,15 +5,21 @@ Check the size of the disk and notify the user if there's not much space left.
 
 import apprise
 import psutil
+from dotenv import load_dotenv
 
 
-secret_string = "Located in an .env file"
+load_dotenv()
+
+bot_token = os.getenv("BOT_TOKEN")
+chat_id = os.getenv("CHAT_ID")
+appr = apprise.Apprise()
+appr.add(f"tgram://{bot_token}/{chat_id}")
+
 min_space = 130 # Minimum amount of space that's acceptable
 space_left = round(psutil.disk_usage("/")[2] / 1024 / 1024 / 1024)
-appr = apprise.Apprise()
-appr.add(f"tgram://{secret_string}?silent=yes")
+
 
 if space_left < min_space:
-    appr.notify(body=f"Minder dan {min_space}GB beschikbaar pik", title="Qbits")
+    appr.notify(body=f"Minder dan {min_space}GB beschikbaar pik")
 else:
     print("nu heb ik niks te zeggen")
