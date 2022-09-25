043dee36f613ae56c42716eda8959bf8e1c5c5d2
Alex de Wekker <github@dewekker.dev>
2022-09-26 00:06:26 +0200

    made some more progress, getting an auth cookie from the qbit api

--- scripts/python/check_disk_space.py
+++ scripts/python/check_disk_space.py
@@ -4,22 +4,52 @@ Check the size of the disk and notify the user if there's not much space left.
 
 
 import apprise
+import os
 import psutil
+import requests as req
 from dotenv import load_dotenv
 
 
 load_dotenv()
 
-bot_token = os.getenv("BOT_TOKEN")
-chat_id = os.getenv("CHAT_ID")
-appr = apprise.Apprise()
-appr.add(f"tgram://{bot_token}/{chat_id}")
 
-min_space = 130 # Minimum amount of space that's acceptable
-space_left = round(psutil.disk_usage("/")[2] / 1024 / 1024 / 1024)
+def apprise_init():
+    """ Initialize the apprise integration. """
+
+    bot_token = os.getenv("BOT_TOKEN")
+    chat_id = os.getenv("CHAT_ID")
+    appr = apprise.Apprise()
+    appr.add(f"tgram://{bot_token}/{chat_id}")
+
+    return appr
+
+def redis_init():
+    print("initialize redis")
+
+def qbit_get_auth_cookie():
+    """ Get the cookie used to authenticate future qbit Api requests. """
+
+    user = os.getenv("QBIT_USER")
+    pw = os.getenv("QBIT_PASS")
+    url = f"http://{os.uname()[1]}.local:8080"
+    res = req.post(
+        url=f"{url}/api/v2/auth/login",
+        data={"username": user, "password": pw},
+        headers={"Referer": url}
+    )
+    return res.headers["set-cookie"].split(";")[0].split("=")[1]
 
 
-if space_left < min_space:
-    appr.notify(body=f"Minder dan {min_space}GB beschikbaar pik")
-else:
-    print("nu heb ik niks te zeggen")
+def main():
+    qbit_cookie = qbit_get_auth_cookie()
+    appr = apprise_init()
+    min_space = 50 # Minimum amount of space that's acceptable
+    space_left = round(psutil.disk_usage("/")[2] / 1024 / 1024 / 1024)
+
+    if space_left < min_space:
+        res = req.post
+        appr.notify(body=f"Serge has less than {min_space}GB available, \
+            the download speed has been set to 500 KB/s")
+
+
+main()
