fd14e80092d4b9fba4d9f862c54adab50da41aa5
Alex de Wekker <github@dewekker.dev>
2023-01-27 21:59:52 +0100

    expanded the script so it now also works with host piet

--- scripts/python/check_updates.py
+++ scripts/python/check_updates.py
@@ -1,6 +1,8 @@
 """
 Check if there are updates and notify.
-File will be executed via cronjob.
+File will be executed via a cronjob.
+
+Currently works on: serge, piet
 """
 
 
@@ -14,14 +16,26 @@ load_dotenv()
 
 bot_token = os.getenv("BOT_TOKEN")
 chat_id = os.getenv("CHAT_ID")
+host = os.uname()[1]
+command = ""
 appr = apprise.Apprise()
 appr.add(f"tgram://{bot_token}/{chat_id}")
 
-pre_updates = subprocess.run(["checkupdates"], capture_output=True)
-updates = int(subprocess.run(["wc", "-l"], input=pre_updates.stdout, capture_output=True).stdout)
+if host == "serge":
+    command = "checkupdates"
+elif host == "piet":
+    command = "aptitude search \"~U\""
+
+pre_updates = subprocess.run([command], capture_output=True)
+updates = int(subprocess.run(
+    ["wc", "-l"],
+    input=pre_updates.stdout,
+    capture_output=True
+).stdout)
+
 print(updates)
 
 if updates == 1:
-    appr.notify(body="Serge has 1 new update 🥸")
+    appr.notify(body=f"{host.capitalize()} has 1 new update 🥸")
 elif updates > 0:
-    appr.notify(body=f"Serge has {updates} new updates 🥸")
+    appr.notify(body=f"{host.capitalize()} has {updates} new updates 🥸")
