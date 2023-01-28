16cec5e496b44c6231f6f39d5ef5a546c21beaf7
Alex de Wekker <github@dewekker.dev>
2023-01-28 15:08:15 +0100

    created a script to change my tasks at midnight so that they're always due today

---
+++ scripts/python/task_date_reset.py
@@ -0,0 +1,30 @@
+"""
+    Resets the date of my todoist tasks.
+    Because I like it that way.
+"""
+
+
+import os
+from dotenv import load_dotenv
+from todoist_api_python.api import TodoistAPI
+
+
+load_dotenv()
+
+api = TodoistAPI(os.getenv("TODOIST_API_KEY"))
+task_ids = []
+
+try:
+    tasks = api.get_tasks()
+    task_ids = [task.id for task in tasks]
+except Exception as error:
+    print(error)
+
+# print(ids)
+
+for task_id in task_ids:
+    try:
+        update = api.update_task(task_id=task_id, due_string="Today")
+        print(f"Task: {task_id} is updated")
+    except Exception as error:
+        print(error)
