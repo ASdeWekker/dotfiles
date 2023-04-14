58df5dfa0791eec8bd1cb17a1e4392a32b4d2f34
Alex de Wekker <github@dewekker.dev>
2023-04-14 23:15:51 +0200

    made a start for a new small automation

---
+++ scripts/python/nc_wa_backup_cleaner.py
@@ -0,0 +1,17 @@
+"""
+This script will remove old WhatsApp backup that are uploaded to Nextcloud.
+"""
+
+
+import os
+
+
+nc_wa_backup_path = "/nextcloud/path"
+
+
+def main():
+    print(nc_wa_backup_path)
+
+
+if __name__ == "__main__":
+    main()
