8f95e41acc0b6f8139e07f8b1d4a1ef3551267e4
Alex de Wekker <github@dewekker.dev>
2023-07-20 22:34:42 +0200

    added a bit of logic to the code

--- scripts/python/task_date_reset.py
+++ scripts/python/task_date_reset.py
@@ -14,15 +14,21 @@ load_dotenv()
 api = TodoistAPI(os.getenv("TODOIST_API_KEY"))
 task_ids = []
 
-try:
-    tasks = api.get_tasks()
-    task_ids = [task.id for task in tasks]
-except Exception as error:
-    print(error)
 
-for task_id in task_ids:
+def main():
     try:
-        update = api.update_task(task_id=task_id, due_string="Today")
-        print(f"Task: {task_id} is updated")
+        tasks = api.get_tasks()
+        task_ids = [task.id for task in tasks]
     except Exception as error:
         print(error)
+
+    for task_id in task_ids:
+        try:
+            update = api.update_task(task_id=task_id, due_string="Today")
+            print(f"Task: {task_id} is updated")
+        except Exception as error:
+            print(error)
+
+
+if __name__ is "__main__":
+    main()
