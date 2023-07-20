c3e993b696621d091daa8bfceb2074c4f3567bf6
Alex de Wekker <github@dewekker.dev>
2023-07-20 22:35:30 +0200

    added a script to wrap around snapraid use, mainly for error checking

---
+++ scripts/python/wrapraid.py
@@ -0,0 +1,19 @@
+"""
+	This will serve as a wrapper for snapraid.
+    The main purpose is to make sure it does not run when an error has been
+    detected in the array.
+"""
+
+
+import os
+
+
+snapraid = "snapraid"
+
+
+def main():
+    print(snapraid)
+
+
+if __name__ is "__main__":
+    main()
