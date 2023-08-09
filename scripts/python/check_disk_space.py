8b45890af762bd0a82a07b67f69d5aa91a96ea1f
Alex de Wekker <github@dewekker.dev>
2023-08-09 23:21:24 +0200

    added some more formatting and comments regarding the disk space

--- scripts/python/check_disk_space.py
+++ scripts/python/check_disk_space.py
@@ -24,6 +24,8 @@ def apprise_init():
     return appr
 
 def redis_init():
+    """ Initialize Redis and set some variables. """
+
     print("initialize redis")
 
 def qbit_get_auth_cookie():
@@ -32,24 +34,35 @@ def qbit_get_auth_cookie():
     user = os.getenv("QBIT_USER")
     pw = os.getenv("QBIT_PASS")
     url = f"http://{os.uname()[1]}.local:8080"
+
+    redis_init()
+
     res = req.post(
         url=f"{url}/api/v2/auth/login",
         data={"username": user, "password": pw},
         headers={"Referer": url}
     )
+
     return res.headers["set-cookie"].split(";")[0].split("=")[1]
 
 
 def main():
-    qbit_cookie = qbit_get_auth_cookie()
-    appr = apprise_init()
+    """ Everything comes together. """
+
     min_space = 50 # Minimum amount of space that's acceptable
     space_left = round(psutil.disk_usage("/")[2] / 1024 / 1024 / 1024)
 
     if space_left < min_space:
-        res = req.post
+        qbit_cookie = qbit_get_auth_cookie()
+        appr = apprise_init()
+
+        res = req.post()
+        
+        print(f"Not enough space available: {space_left}GB")
         appr.notify(body=f"Serge has less than {min_space}GB available, \
             the download speed has been set to 500 KB/s")
+    else:
+        print(f"Enough space available: {space_left}GB")
 
 
 if __name__ == "__main__":
