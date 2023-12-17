9230b9ebed3e49ee7a641bbe902e2c3ff60b3124
Alex de Wekker <github@dewekker.dev>
2023-12-17 16:15:31 +0100

    got a working check disk script, added some older code for later review

--- scripts/python/automations/check_disk_space.py
+++ scripts/python/automations/check_disk_space.py
@@ -15,124 +15,137 @@ load_dotenv()
 
 
 url = f"http://{os.uname()[1]}.local:8080/api/v2"
-
-
-def redis_init():
-    """ Initialize Redis and set some variables. """
-
-    print("initialize redis")
-
-
-def qbit_get_auth_cookie():
-    """ Get the cookie used to authenticate future qbit Api requests. """
-
-    user = os.getenv("QBIT_USER")
-    pw = os.getenv("QBIT_PASS")
-
-    headers = {"Referer": url}
-
-    res = req.post(
-        url=f"{url}/auth/login",
-        data={"username": user, "password": pw},
-        headers=headers
-    )
-
-    if res.status_code == 200:
-        print("Logged in!")
-        return res.headers["set-cookie"].split(";")[0].split("=")[1]
-    print("Something went wrong while trying to login.")
-
-
-def torrent_list(filters, sort, reverse):
-    """ Get a list of all the torrents currently in Qbit. """
-
-    # cookies = {"SID": qbit_get_auth_cookie()}
-    params = {
-        "filter": filters,
-        "sort": sort,
-        "reverse": reverse,
-    }
-
-    res = req.get(
-        url=f"{url}/torrents/info",
-        # headers=headers,
-        cookies={"SID": qbit_get_auth_cookie()},
-        params=params
-    )
-
-    if res.status_code == 200:
-        return res.json()
-
-
-def logout():
-    """ Logout once everything is done. """
-
-    # cookies = {"SID": qbit_get_auth_cookie()}
-
-    res = req.post(
-        url=f"{url}/auth/logout",
-        # headers=headers,
-        cookies={"SID": qbit_get_auth_cookie()}
-    )
-
-    if res.status_code == 200:
-        print("Logged out again.")
-    else:
-        print("Something went wrong while logging out.")
+user = os.getenv("QBIT_USER")
+pw = os.getenv("QBIT_PASS")
+headers = {"Referer": f"http://{os.uname()[1]}.local:8080"}
 
 
 def main():
     """ Everything comes together. """
 
-    min_space = 150  # Minimum amount of space that's acceptable in GB.
+    # Some more vars.
+    min_space = 50  # Minimum amount of space that's acceptable in GB.
     speed_limit = 500  # The imposed speedlimit in KB/s.
     space_left = round(psutil.disk_usage("/")[2] / 1024 / 1024 / 1024)
-
-    # torrents = torrent_list("all", "ratio", "false")
+    cookie = ""
+    data = []
 
     if space_left < min_space:
-        cookies = {"SID": qbit_get_auth_cookie()}
-        torrents = torrent_list("all", "ratio", "false")
-        params = {
-            # "hashes": [torrent["hash"] for torrent in torrents].join("|"),
-            "hashes": "|".join([torrent["hash"] for torrent in torrents]),
-            "limit": speed_limit * 1024,
-        }
-        headers = {
-            "Content-Type": "application/x-www-form-urlencoded"
-        }
+        print("Not enough space left.")
 
-        # for torrent in torrents:
+        # Login.
         res = req.post(
-            url=f"{url}/torrents/setDownloadLimit",
-            headers=headers,
-            cookies=cookies,
-            params=params
+            url=f"{url}/auth/login",
+            data={"username": user, "password": pw},
+            headers=headers
         )
 
-        # res = req.post(
-        #     url=f"{url}/transfer/setDownloadLimit",
-        #     data={"limit": speed_limit * 1024},
-        #     headers=headers,
-        #     cookies=cookies
-        # )
+        if res.ok:
+            print("Logged in!")
+            cookie = res.headers["set-cookie"].split(";")[0].split("=")[1]
+        else:
+            print("Something went wrong while trying to login.")
 
-        if res.status_code == 200:  # Not enough space.
-            print(f"Not enough space available: {space_left}GB, "
-                  f"limit is set to {min_space}GB.")
-            telmes.message(f"Serge has less than {min_space}GB available, "
-                           f"limit is set to {min_space}GB. The download "
-                           f"speed has been set to {speed_limit} KB/s.")
-        else:  # Something went wrong limiting the speed.
-            print("Something went wrong :(")
-            print(res.status_code)
-            telmes.message("Something went wrong while trying to limit the "
-                           "download speed.")
-    else:  # Enough space available.
-        print(f"Enough space available: {space_left}GB, "
-              f"limit is set to {min_space}GB.")
+        # Get list of torrents.
+        res = req.get(
+            url=f"{url}/torrents/info",
+            headers=headers,
+            cookies={"SID": cookie},
+            params={"filter": "all", "sort": "ratio"}
+        )
 
-    logout()
+        if res.ok:
+            data = res.json()
+        else:
+            print("Something went wrong while grabbing the torrents. "
+                  f"Status code: {res.status_code}.")
+
+        # Slow the torrents down.
+        params = {
+            "hashes": "|".join([item["hash"] for item in data]),
+            "limit": str(speed_limit * 1024)
+        }
+
+        res = req.post(
+            url=f"{url}/torrents/setDownloadLimit",
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+            cookies={"SID": cookie},
+            data=params
+        )
+
+        if res.ok:
+            print("Slowed the torrents down")
+            telmes.message(f"Slowed down the torrents to {speed_limit} KB/s")
+        else:
+            print(f"Status code: {res.status_code}")
+            print(res.text)
+            telmes.message("Something went wrong while trying to slow down.")
+
+        # Logout again.
+        res = req.post(
+            url=f"{url}/auth/logout",
+            headers=headers,
+            cookies={"SID": cookie}
+        )
+
+        if res.ok:
+            print("Logged out again.")
+        else:
+            print("Something went wrong while trying to logout.")
+    else:
+        print(f"Enough space left: {space_left}GB, "
+              "threshold is {min_space}GB, skipping...")
+
+# def main():
+#     """ Everything comes together. """
+
+#     min_space = 150  # Minimum amount of space that's acceptable in GB.
+#     speed_limit = 500  # The imposed speedlimit in KB/s.
+#     space_left = round(psutil.disk_usage("/")[2] / 1024 / 1024 / 1024)
+
+#     # torrents = torrent_list("all", "ratio", "false")
+
+#     if space_left < min_space:
+#         cookies = {"SID": qbit_get_auth_cookie()}
+#         torrents = torrent_list("all", "ratio", "false")
+#         params = {
+#             # "hashes": [torrent["hash"] for torrent in torrents].join("|"),
+#             "hashes": "|".join([torrent["hash"] for torrent in torrents]),
+#             "limit": speed_limit * 1024,
+#         }
+#         headers = {
+#             "Content-Type": "application/x-www-form-urlencoded"
+#         }
+
+#         # for torrent in torrents:
+#         res = req.post(
+#             url=f"{url}/torrents/setDownloadLimit",
+#             headers=headers,
+#             cookies=cookies,
+#             params=params
+#         )
+
+#         # res = req.post(
+#         #     url=f"{url}/transfer/setDownloadLimit",
+#         #     data={"limit": speed_limit * 1024},
+#         #     headers=headers,
+#         #     cookies=cookies
+#         # )
+
+#         if res.status_code == 200:  # Not enough space.
+#             print(f"Not enough space available: {space_left}GB, "
+#                   f"limit is set to {min_space}GB.")
+#             telmes.message(f"Serge has less than {min_space}GB available, "
+#                            f"limit is set to {min_space}GB. The download "
+#                            f"speed has been set to {speed_limit} KB/s.")
+#         else:  # Something went wrong limiting the speed.
+#             print("Something went wrong :(")
+#             print(res.status_code)
+#             telmes.message("Something went wrong while trying to limit the "
+#                            "download speed.")
+#     else:  # Enough space available.
+#         print(f"Enough space available: {space_left}GB, "
+#               f"limit is set to {min_space}GB.")
 
 
 if __name__ == "__main__":
